185ab0879250cb2133d58feee7333ac5362c446a
Timothy Ryon <tim@timryon.com>
2024-07-22 19:23:05 -0400

    wip

--- server/find_first_duplicate.py
+++ server/find_first_duplicate.py
@@ -2,12 +2,19 @@ def find_first_duplicate(num_list):
     eleCount = dict()
 
     for num in num_list:
-        eleCount[num_list[num]] = eleCount.get(num_list[num], 1) + 1
-        k = list(eleCount.keys())
-        v = list(eleCount.values())
-        if 2 in v:
-            return (k[v.index(v)])
+        eleCount[num] = eleCount.get(num, 0) + 1
+        value ={i for i in eleCount if eleCount[i] == 2}
+        # k = list(eleCount.keys())
+        # v = list(eleCount.values())
+        breakpoint()
+        if value == 2:
+            return (list(eleCount.keys())[list(eleCount.values()).index(2)])
+            # return (k[v.index(v)])
         else:
             return -1
 
-print(find_first_duplicate([2, 1, 3, 3, 2]))
+    
+
+
+num_list = [2, 1, 3, 3, 2]
+print(find_first_duplicate(num_list))
