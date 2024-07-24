2a2d42b624e5835f08c07ab74c2a5cefa9af53c5
Timothy Ryon <tim@timryon.com>
2024-07-23 21:05:58 -0400

    finish py solution and passes all my tests

--- server/find_first_duplicate.py
+++ server/find_first_duplicate.py
@@ -4,17 +4,20 @@ def find_first_duplicate(num_list):
     for num in num_list:
         eleCount[num] = eleCount.get(num, 0) + 1
         value ={i for i in eleCount if eleCount[i] == 2}
-        # k = list(eleCount.keys())
-        # v = list(eleCount.values())
-        breakpoint()
-        if value == 2:
+        if value:
             return (list(eleCount.keys())[list(eleCount.values()).index(2)])
-            # return (k[v.index(v)])
         else:
-            return -1
+            continue
+        
+    return -1
 
     
 
 
-num_list = [2, 1, 3, 3, 2]
+# num_list = []
+# num_list = [4]
+# num_list = [1, 2, 3, 4]
+# num_list = [1, 2, 3, 2, 1]
+# num_list = [2, 1, 3, 3, 2]
+num_list = [1, 2, 3, 3, 2, 1]
 print(find_first_duplicate(num_list))
